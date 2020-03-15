345d4d16e2af26a1793eca57084a7647950fe76a
akashkshekarappa <33307773+akashkshekarappa@users.noreply.github.com>
2020-03-14 22:10:02 -0400

    Services for cross component communications

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import {LoggingService} from '../logging.service';
+import {AccountService} from '../account.service';
 
 @Component({
   selector: 'app-new-account',
@@ -7,14 +8,15 @@ import {LoggingService} from '../logging.service';
   styleUrls: ['./new-account.component.css']
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  constructor(private  accountService: AccountService) {
+    this.accountService.statusUpdated.subscribe(
+      (status: string) => alert('New Status: ' + status)
+    );
+  }
+
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-    const service = new LoggingService();
-    service.logStatusChange(accountStatus);
+    this.accountService.addAccount(accountName, accountStatus);
   }
 }
